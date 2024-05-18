f16757d0a35d3577005cdee587587da77c63d39e
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2024-05-18 23:01:31 +0500

    Implement Initial Project Setup

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Welcome = () => {
+  return (
+    <View className="bg-white">
+      <Text className="text-lg text-custom-pink-400">Heelooo</Text>
+      <Text className="text-blue-500 text-2xl font-bold mb-24">Doctor Assisdddtance</Text>
+    </View>
+  )
+}
+
+export default Welcome
